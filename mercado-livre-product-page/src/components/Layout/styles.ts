3f2b5dc3aeb1c23d4fb80ded93e1d995f6100615
luiquecruz <luiquecruz@gmail.com>
2020-10-04 21:06:18 -0300

    ML product page clone init

---
+++ mercado-livre-product-page/src/components/Layout/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Wrapper = styled.div`
+  max-width: 1220px;
+  padding: 0 16px;
+  margin: 0 auto;
+`;
---
+++ mercado-livre-product-page/src/components/Product/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Product: React.FC = () => {
+  return (
+    <Container>
+      <h2>Product</h2>
+    </Container>
+  );
+};
+
+export default Product;
